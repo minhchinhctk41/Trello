25b42ab14618186dcb296dee6529405e725013e8
minhchinhctk41 <minhchinh04@gmail.com>
2021-08-26 23:04:39 +0700

    Implement Darg and Drop, eslint.

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,36 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import './BoardContent.scss';
+import React, { useState, useEffect } from 'react'
+import { Container, Draggable } from 'react-smooth-dnd'
+import './BoardContent.scss'
 
-import { isEmpty } from 'lodash';
+import { isEmpty } from 'lodash'
 
-import Column from 'components/Column/Column';
-import { mapOrder } from 'utilities/sorts';
+import Column from 'components/Column/Column'
+import { mapOrder } from 'utilities/sorts'
 
-import {initialData} from 'actions/initialData';
+import { initialData } from 'actions/initialData'
 
 
 function BoardContent() {
 
-    const [board, setBoard] = useState({});
-    const [columns, setColumns] = useState([]);
-    useEffect(() => {
-        const boardFromDB = initialData.boards.find(board =>board.id === 'board-1')
-        if (boardFromDB) {
-            setBoard(boardFromDB)
-            
-            setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
-        }
-    }, [])
+  const [board, setBoard] = useState({})
+  const [columns, setColumns] = useState([])
+  useEffect(() => {
+    const boardFromDB = initialData.boards.find(board => board.id === 'board-1')
+    if (boardFromDB) {
+      setBoard(boardFromDB)
 
-    if (isEmpty(board)) {
-        return <div className="not-found" style={{'padding': '10px', 'color': 'white'}}>Board not found!</div>
+      setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'))
     }
+  }, [])
 
-    return (
-        <div className="board-content">
-            {columns.map((column, index) => <Column key={index} column={column} />)}
-        </div>
-    )
+  if (isEmpty(board)) {
+    return <div className="not-found" style={{ 'padding': '10px', 'color': 'white' }}>Board not found!</div>
+  }
+
+  const onColumnDrop = (dropResult) => {
+    console.log(dropResult)
+  }
+
+  return (
+    <div className="board-content">
+      <Container
+        orientation="horizontal"
+        onDrop={onColumnDrop}
+
+        getChildPayload={index => columns[index]}
+
+        dragHandleSelector=".column-drag-handle"
+        dropPlaceholder={{
+          animationDuration: 150,
+          showOnTop: true,
+          className: 'column-drop-preview'
+        }}
+      >
+        {columns.map((column, index) => (
+          <Draggable key={index}>
+            <Column column={column} />
+          </Draggable>
+        ))}
+      </Container>
+    </div>
+  )
 }
 
-export default BoardContent;
+export default BoardContent
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from 'react'
 
-import './Card.scss';
+import './Card.scss'
+import img from 'Images/avatar.png'
 
 function Card(props) {
-    const {card} = props
-    return (
-        <li className="card-item">
-            {card.cover && <img src={card.cover} alt="mc" className="card-cover"></img>}
-            
-            {card.title}
-        </li>
-    );
+  const { card } = props
+  return (
+    <div className="card-item">
+      {card.cover &&
+      <img src={card.cover}
+        alt="mc"
+        className="card-cover"
+        onMouseDown={e => e.preventDefault()}>
+      </img>
+      }
+
+      {card.title}
+    </div>
+  )
 }
 
-export default Card;
+export default Card
