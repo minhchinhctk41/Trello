b3510a2bf4e70ae17dae86d03e646c2068cec36b
minhchinhctk41 <minhchinh04@gmail.com>
2021-09-05 18:19:50 +0700

    Deploy Trello to firebase.

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,5 +1,6 @@
 
-export const API_ROOT ='http://localhost:8000'
+//export const API_ROOT ='http://localhost:8000'
+export const API_ROOT ='https://trello-api-mc.herokuapp.com'
 
 export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE'
 export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM'
